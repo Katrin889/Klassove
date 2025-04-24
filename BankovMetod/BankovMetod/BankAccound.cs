5abd46379c75d1b1f0d966d370581e56c4b49839
Katrin889 <kakamitreva@gmail.com>
2025-04-24 09:45:25 +0300

    Add files via upload

---
+++ BankovMetod/BankovMetod/BankAccound.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankovMetod
+{
+    internal class BankAccound
+    {
+        private int id;
+        private double balance;
+        //private int create;
+        //private int print;
+        //private int end;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        //public int Create
+        //{
+        //    get { return create; }
+        //    set { create = value; }
+        //}
+        //public int Print
+        //{
+        //    get { return print; }
+        //    set { print = value; }
+        //}
+        //public int End
+        //{
+        //    get { return end; }
+        //    set { end = value; }
+        //}
+        public void Deposit(double amount)
+        {
+            if (amount < 0) throw new ArgumentException("Invalid deposit amound");
+                this.balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount < balance) throw new InvalidOperationException("Insufficient balance");
+                this.balance -= amount;
+        }
+    
+        public override string ToString()
+        {
+            return $"Accound {this.id}, balance {this.balance}";
+        }
+        public BankAccound(int id)
+        {
+            ID = id;
+            Balance = 0;
+        }
+    }
+}
---
+++ BankovMetod/BankovMetod/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankovMetod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BankAccound acc = new BankAccound();
+            //acc.ID = 2;
+            //acc.Deposit(200);
+            //acc.Withdraw(20);
+            //Console.WriteLine(acc.ToString());
+
+            Dictionary<int, BankAccound> accounds = new Dictionary<int, BankAccound>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] parts = input.Split();
+                string command = parts[0];
+                int id = int.Parse(parts[1]);
+                try
+                {
+                    switch (command)
+                    {
+                        case "Create":
+                            if (!accounds.ContainsKey(id))
+                                accounds[id] = new BankAccound(id);
+                            else
+                                Console.WriteLine("Accound already exists");
+                            break;
+                        case "Deposit":
+                            double depositAmound = double.Parse(parts[2]);
+                            if (accounds.ContainsKey(id))
+                                accounds[id].Deposit(depositAmound);
+                            else
+                                Console.WriteLine("Accound already exists");
+                            break;
+                        case "Withdraw":
+                            double withdrawAmound = double.Parse(parts[2]);
+                            if (accounds.ContainsKey(id))
+                                accounds[id].Deposit(withdrawAmound);
+                            else
+                                Console.WriteLine("Accound already exists");
+                            break;
+                        case "Print":
+                            if (accounds.ContainsKey(id))
+                                Console.WriteLine(accounds[id]);
+                            else
+                                Console.WriteLine("Accound already exists");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid command");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
